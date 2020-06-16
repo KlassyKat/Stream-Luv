cb0d85eefd132bde68f164edcd4e801402f7fb73
gabealexander <gabealexander01@gmail.com>
2020-06-16 00:19:54 -0400

    hover css

--- draggable.js
+++ draggable.js
@@ -1,68 +1,3 @@
-// function setDraggables() {
-//     const draggables = document.querySelectorAll('.draggable');
-//     const container = document.getElementById('tile-wrapper');
-
-//     draggables.forEach(draggable => {
-//         draggable.addEventListener('mousedown', (e) => {
-//             draggable.classList.add('dragging');
-//             draggable.style.x = e.clientX;
-//             draggable.style.y = e.clientY;
-//         });
-
-//         draggable.addEventListener('mouseup', (e) => {
-//             draggable.classList.remove('dragging');
-//             sortStreamers();
-//         });
-//     });
-
-//     container.addEventListener('dragover', (e) => {
-//         e.preventDefault();
-//         console.log(e);
-//         const afterElement = getAfterElement(container, e.clientY);
-//         const draggable = document.querySelector('.dragging');
-//         if (afterElement == null) {
-//             container.appendChild(draggable);
-//         } else {
-//             container.insertBefore(draggable, afterElement);
-//         }
-//     });
-//     document.querySelector("body").addEventListener('dragover', (e) => {
-//         e.preventDefault();
-//     })
-
-
-//     function getAfterElement(wrapper, y) {
-//         const draggableElements = [...wrapper.querySelectorAll('.draggable:not(.dragging)')];
-
-//         return draggableElements.reduce((closest, child) => {
-//             const box = child.getBoundingClientRect();
-//             const offset = y - box.top - box.height / 1.8;
-//             if (offset < 0 && offset > closest.offset) {
-//                 return {
-//                     offset: offset,
-//                     element: child
-//                 };
-//             } else {
-//                 return closest;
-//             }
-//         }, {
-//             offset: Number.NEGATIVE_INFINITY
-//         }).element;
-//     }
-
-//     function sortStreamers() {
-//         let streamOrderElements = [...document.querySelectorAll('.stream-item')];
-//         let streamOrder = {};
-//         for(element of streamOrderElements) {
-//             if(streamers[element.id]) {
-//                 streamOrder[element.id] = (streamers[element.id]);
-//             }
-//         }
-//         localStorage.setItem('streamers', JSON.stringify(streamOrder));
-//         streamers = JSON.parse(localStorage.getItem('streamers'));
-//     }
-// }
-
 function setDraggables() {
     const draggables = document.querySelectorAll('.draggable');
     const container = document.getElementById('tile-wrapper');
@@ -99,7 +34,7 @@ function setDraggables() {
 
         return draggableElements.reduce((closest, child) => {
             const box = child.getBoundingClientRect();
-            const offset = y - box.top - box.height / 1.8;
+            const offset = y - box.top - box.height / 2;
             if (offset < 0 && offset > closest.offset) {
                 return {
                     offset: offset,
